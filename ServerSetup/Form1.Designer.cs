65f8ea8ef040f7efa65b4f3d02ca98c1c8fb1647
Dylon Lowinske <dylon.lowinske@gmail.com>
2022-11-18 16:03:27 -0600

    Initial Form Layout

--- ServerSetup/Form1.Designer.cs
+++ ServerSetup/Form1.Designer.cs
@@ -28,12 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.OpenServerFolder = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Title = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // OpenServerFolder
+            // 
+            this.OpenServerFolder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(82)))), ((int)(((byte)(82)))));
+            this.OpenServerFolder.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.OpenServerFolder.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.OpenServerFolder.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.OpenServerFolder.Location = new System.Drawing.Point(619, 71);
+            this.OpenServerFolder.Name = "OpenServerFolder";
+            this.OpenServerFolder.Size = new System.Drawing.Size(151, 27);
+            this.OpenServerFolder.TabIndex = 0;
+            this.OpenServerFolder.Text = "Open Server Folder";
+            this.OpenServerFolder.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.OpenServerFolder.UseVisualStyleBackColor = false;
+            this.OpenServerFolder.Click += new System.EventHandler(this.OpenServerFolder_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(82)))), ((int)(((byte)(82)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.textBox2.Location = new System.Drawing.Point(12, 71);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(601, 27);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Title.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Title.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Title.Location = new System.Drawing.Point(320, 9);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(181, 48);
+            this.Title.TabIndex = 2;
+            this.Title.Text = "Brett.AI";
+            this.Title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Title.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(49)))), ((int)(((byte)(49)))));
+            this.ClientSize = new System.Drawing.Size(782, 453);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.OpenServerFolder);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button OpenServerFolder;
+        private TextBox textBox2;
+        private Label Title;
     }
 }
--- ServerSetup/Form1.cs
+++ ServerSetup/Form1.cs
@@ -2,9 +2,44 @@ namespace ServerSetup
 {
     public partial class Form1 : Form
     {
+        public string ServerFolderDir = @"C:\Users\dylon\OneDrive\Documents\Server";
+        public string[] Files { get; set; }
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void OpenServerFolder_Click(object sender, EventArgs e)
+        {
+            using (var browserDialog = new FolderBrowserDialog())
+            {
+                browserDialog.InitialDirectory = ServerFolderDir;
+                DialogResult result = browserDialog.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(browserDialog.SelectedPath))
+                {
+                    Files = Directory.GetFiles(browserDialog.SelectedPath);
+
+                    //System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+                }
+                foreach(var file in Files)
+                {
+                    Console.Write(file);
+
+                }
+                //Console.Write(browserDialog.SelectedPath);
+                textBox2.Text = browserDialog.SelectedPath;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
